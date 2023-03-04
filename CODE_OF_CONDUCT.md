1a01be8eda37f48f8bf37f093e12c5d3bd71a3fe
Casey Marshall <git@cmars.tech>
2023-03-03 22:04:23 -0600

    chore: update nix shell, conduct
    Nix setup has not aged well, Go 1.17 is well EOL. Update to 1.19.
    
    Code of conduct also has not aged well. The world has become an even
    darker place.
    
    Try to be excellent to each other. We all need it.

--- CODE_OF_CONDUCT.md
+++ CODE_OF_CONDUCT.md
@@ -2,6 +2,4 @@
 
 If you really need one:
 
-- Don't be a jerk.
-- Don't propose anything illegal.
-- Don't make me have to decide what any of that means. Nobody got time for that.
+- Be excellent to each other.
--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     nativeBuildInputs = with pkgs.buildPackages; [
-      go_1_17 tor openssl_1_1 libevent zlib goreleaser podman podman-compose
+      go_1_19 tor openssl_1_1 libevent zlib goreleaser podman podman-compose
     ];
     shellHook = ''
       export GOPATH="$HOME/.cache/gopaths/$(sha256sum <<<$(pwd) | awk '{print $1}')"
